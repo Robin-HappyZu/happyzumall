58f4ed4a6da87bca0b78be4ae8656b235d113b0c
RobinZ. <94353156@qq.com>
2016-12-19 16:02:27 +0800

    订单Model

---
+++ Happyzu.Mall.Core/Orders/OrderItemRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+
+namespace Happyzu.Mall.Orders
+{
+    public class OrderItemRecord:Entity<long>, ICreationAudited
+    {
+        /// <summary>
+        /// 订单编号
+        /// </summary>
+        public string OrderNo { get; set; }
+        /// <summary>
+        /// 商品Id
+        /// </summary>
+        public int ProductId { get; set; }
+
+        public string SKU { get; set; }
+
+
+
+        #region ICreationAudited
+        public DateTime CreationTime { get; set; }
+        public long? CreatorUserId { get; set; }
+        #endregion
+    }
+}
